90de0351474aaf786574576d31ae63225f1389c1
Dontavious Green <dontavious.o.green@gmail.com>
2022-10-15 13:57:00 -0500

    Errors operational but 'ultimate' condition to create event isn't working correctly. Addressing the issue. 4 correct 6 bugged.Adding update logic. making update event logic. Need to setup sub to get data from service into userData. now importing old event into update fields

--- src/app/update-event/update-event.component.ts
+++ src/app/update-event/update-event.component.ts
@@ -11,87 +11,41 @@ import { UpdateEventService } from '../update-event.service';
   styleUrls: ['./update-event.component.css']
 })
 export class UpdateEventComponent implements OnInit {
-  
-updatedInfo:any = [];
-newEventInfo = []
-oldName:string = "";
 
-eventData!:IEvents[];
-
-userData = {
-    id:"",
-    eventName: "David",
-    meetingDate: "2022-10-13",
-    eventTimeStart: "12:00",
-    eventTimeEnd: "12:30",
-    location: "Minnesota",
-    ContactPersonName: "Donny",
-    ContactPersonNumber: 123456789,
-    ContactPersonEmail: "donnyG@gmail.com",
-    costToAttend: 0,
-    notes: "Have Mad Fun"
-}
-
+    //Imported event Info from Db to be updated
+    updatedEventDataValues = 
+        {
+        id:"",
+    eventName: "",
+    meetingDate: "",
+    eventTimeStart: "",
+    eventTimeEnd: "",
+    location: "",
+    contactPersonName: "",
+    contactPersonNumber: "",
+    contactPersonEmail: "",
+    costToAttend: "",
+    notes: ""
+};
 
 
   constructor(private eventService: EventsService,
               private updateEventService: UpdateEventService,
               private httpService: HttpService) {
 
-    // this.updateEventService.$oldEventName.subscribe(
-    //     (currentEventInfo) => {
-    //       this.eventData = currentEventInfo
-    //       console.log(this.eventData)
-    //     });
-
-    // this.updateEventService.updatedEventData.
-    // // pipe(takeUntil(this.onDestroy)).
-    // subscribe(eventName => this.eventData = eventName
-    // );
-
-    //retrieve data from observable(promise)
-    this.httpService.getEvents()
-        // pipe(first())
-        .subscribe( {
-          //this func is executed if data is received
-          next: (data) => {
-            console.log(data)
-            this.eventData = data;
-            console.log(this.eventData)
-          },
-          //this func is executed if request fails
-          error: (error) => {
-            console.log(error)
-            //this.errorMessagesServer = err;
-          }
-        })
-
+      // @ts-ignore
+      this.updatedEventDataValues = this.updateEventService.updatedEventData
+      console.log(this.updatedEventDataValues)
 
 
   }
 
   onDestroy(onDestroy: any): any {
-        throw new Error('Method not implemented.');
+        
     }
 
 
-  // eventName: string;
-
   ngOnInit(): void {
-
-    // this.newEventInfo = {
-    //
-    //   eventName: "late",
-    // meetingDate: "12-3-22",
-    // eventTimeStart: "12-00",
-    // eventTimeEnd: "12-30",
-    // location: "MN",
-    // ContactPersonName: "Doug",
-    // ContactPersonNumber: "123-45-6789",
-    // ContactPersonEmail: "douglife@gmail.com",
-    // costToAttend: 0,
-    // notes: "Have Mad Fun",
-    // }
 
   }
 
@@ -102,13 +56,10 @@ userData = {
 
     }
 
-  onUpEventClick(eventForm: any) {
+  onUpdateEventClick(eventForm:IEvents) {
     console.log("retrieving data")
     console.log(eventForm)
-    this.updateEventService.updatedEventData = this.updatedInfo;
-    console.log("rec. data from service")
-    console.log(this.updatedInfo)
 
-    
+      
   }
 }
